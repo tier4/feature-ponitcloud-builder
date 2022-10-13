e95df81b414b38b4fd9eca4d9c7c187ad9d2438b
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-10-13 20:48:57 +0900

    Implement surface extraction

--- src/main.cpp
+++ src/main.cpp
@@ -39,11 +39,19 @@ Eigen::Vector3d xyz_as_vector3d(const PointT & p)
 }
 
 template<typename PointT>
-bool has_edge(const typename pcl::VoxelGridCovariance<PointT>::LeafConstPtr & leaf)
+bool has_edge(const Eigen::Vector3d & eigenvalues, const double eigenvalue_ratio)
 {
   return
-    leaf->evals_(2) > leaf->evals_(0) * 6.0 &&
-    leaf->evals_(2) > leaf->evals_(1) * 3.0;
+    eigenvalues(2) > eigenvalues(0) * eigenvalue_ratio &&
+    eigenvalues(2) > eigenvalues(1) * eigenvalue_ratio;
+}
+
+template<typename PointT>
+bool has_surface(const Eigen::Vector3d & eigenvalues, const double eigenvalue_ratio)
+{
+  return
+    eigenvalues(0) < eigenvalues(2) * eigenvalue_ratio &&
+    eigenvalues(0) < eigenvalues(1) * eigenvalue_ratio;
 }
 
 inline double mahalanobis(
@@ -59,6 +67,12 @@ inline Eigen::Vector3d principal(const Eigen::Matrix3d & eigenvectors)
   return eigenvectors.col(2);
 }
 
+inline double point_plane_distance(const Eigen::VectorXd & w, const Eigen::VectorXd & x)
+{
+  assert(w.size() == x.size());
+  return std::fabs(w.dot(x)) / w.norm();
+}
+
 template<typename PointT>
 bool has_valid_covariance(
   const typename pcl::VoxelGridCovariance<PointT>::LeafConstPtr & leaf)
@@ -66,7 +80,36 @@ bool has_valid_covariance(
   return leaf->nr_points >= 0;
 }
 
-constexpr double max_mahalanobis = 0.6;
+Eigen::Vector3d surface_normal(const Eigen::Matrix3d & eigenvectors)
+{
+  // eigen vector corresponding to the smallest eigen value
+  return eigenvectors.col(0);
+}
+
+template<typename PointT>
+double point_plane_distance(
+  const typename pcl::VoxelGridCovariance<PointT>::LeafConstPtr & leaf,
+  const pcl::PointXYZ & p)
+{
+  const Eigen::Vector3d d = xyz_as_vector3d(p) - leaf->mean_;
+  return point_plane_distance(surface_normal(leaf->evecs_), d);
+}
+
+template<typename PointT>
+double edge_point_distance(
+  const typename pcl::VoxelGridCovariance<PointT>::LeafConstPtr & leaf, const pcl::PointXYZ & p)
+{
+  const Eigen::Vector3d diff = xyz_as_vector3d(p) - leaf->mean_;
+  return mahalanobis(leaf->icov_, principal(leaf->evecs_), diff);
+}
+
+constexpr double edge_neighbor_threshold = 1.0;
+constexpr double surface_neighbor_threshold = 1.0;
+constexpr double max_mahalanobis = 1.0;
+constexpr double voxel_size = 1.0;
+constexpr int min_points_per_voxel = 10;
+constexpr double edge_eigenvalue_ratio = 8.0;
+constexpr double surface_eigenvalue_ratio = 0.1;
 
 int main(int argc, char * argv[])
 {
@@ -77,13 +120,13 @@ int main(int argc, char * argv[])
 
   using PointT = pcl::PointXYZ;
 
-  const double voxel_size = 1.0;
-  const int min_points_per_voxel = 20;
   const Eigen::Vector3d voxel_sizes(voxel_size, voxel_size, voxel_size);
+
   const auto pcd_cloud = read_pcd<PointT>(argv[1]);
   auto voxels = crop_into_voxels<PointT>(pcd_cloud, voxel_sizes, min_points_per_voxel);
 
-  pcl::PointCloud<PointT>::Ptr filtered(new pcl::PointCloud<PointT>);
+  pcl::PointCloud<PointT>::Ptr edge(new pcl::PointCloud<PointT>);
+  pcl::PointCloud<PointT>::Ptr surface(new pcl::PointCloud<PointT>);
   for (auto p : *pcd_cloud) {
     const pcl::VoxelGridCovariance<PointT>::LeafConstPtr leaf = voxels.getLeaf(p);
 
@@ -91,20 +134,23 @@ int main(int argc, char * argv[])
       continue;
     }
 
-    if (has_valid_covariance<PointT>(leaf)) {
+    if (!has_valid_covariance<PointT>(leaf)) {
+      continue;
     }
 
-    if (!has_edge<PointT>(leaf)) {
-     continue;
+    if (has_edge<PointT>(leaf->evals_, edge_eigenvalue_ratio) &&
+        edge_point_distance<PointT>(leaf, p) < edge_neighbor_threshold) {
+      edge->push_back(p);
     }
 
-    const Eigen::Vector3d diff = xyz_as_vector3d(p) - leaf->mean_;
-    if (mahalanobis(leaf->icov_, principal(leaf->evecs_), diff) < max_mahalanobis) {
-      filtered->push_back(p);
+    if (has_surface<PointT>(leaf->evals_, surface_eigenvalue_ratio) &&
+        point_plane_distance<PointT>(leaf, p) < surface_neighbor_threshold) {
+      surface->push_back(p);
     }
   }
 
-  pcl::io::savePCDFile<PointT>(std::string{"filtered.pcd"}, *filtered, true);
+  pcl::io::savePCDFile<PointT>(std::string{"edge.pcd"}, *edge, true);
+  pcl::io::savePCDFile<PointT>(std::string{"surface.pcd"}, *surface, true);
 
   return 0;
 }

a019ac789ad7ee321b5d1ed8437576a6366fa60c
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-11-09 19:41:03 +0900

    Add functions to create edge and surface maps

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Eigen/Core>
 
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
+#include <string>
 
 #include <pcl/filters/voxel_grid_covariance.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/common/io.h>
 #include <pcl/point_types.h>
 
 template<typename PointT>
@@ -103,12 +107,62 @@ double edge_point_distance(
   return mahalanobis(leaf->icov_, principal(leaf->evecs_), diff);
 }
 
-constexpr double edge_neighbor_threshold = 1.0;
-constexpr double surface_neighbor_threshold = 1.0;
-constexpr double max_mahalanobis = 1.0;
+template<typename PointT>
+bool is_valid(const typename pcl::VoxelGridCovariance<PointT>::LeafConstPtr & leaf) {
+  return leaf != nullptr && has_valid_covariance<PointT>(leaf);
+}
+
+template<typename PointT>
+typename pcl::PointCloud<PointT>::Ptr ExtractEdge(
+  typename pcl::VoxelGridCovariance<PointT> & voxels,
+  const typename pcl::PointCloud<PointT>::Ptr & input_cloud,
+  const double edge_eigenvalue_ratio,
+  const double edge_neighbor_threshold)
+{
+  typename pcl::PointCloud<PointT>::Ptr edge(new pcl::PointCloud<PointT>);
+  for (auto p : *input_cloud) {
+    const auto leaf = voxels.getLeaf(p);
+
+    if (!is_valid<PointT>(leaf)) {
+      continue;
+    }
+
+    if (has_edge<PointT>(leaf->evals_, edge_eigenvalue_ratio) &&
+        edge_point_distance<PointT>(leaf, p) < edge_neighbor_threshold) {
+      edge->push_back(p);
+    }
+  }
+  return edge;
+}
+
+template<typename PointT>
+typename pcl::PointCloud<PointT>::Ptr ExtractSurface(
+  typename pcl::VoxelGridCovariance<PointT> & voxels,
+  const typename pcl::PointCloud<PointT>::Ptr & input_cloud,
+  const double surface_eigenvalue_ratio,
+  const double surface_neighbor_threshold)
+{
+  typename pcl::PointCloud<PointT>::Ptr surface(new pcl::PointCloud<PointT>);
+  for (auto p : *input_cloud) {
+    const auto leaf = voxels.getLeaf(p);
+
+    if (!is_valid<PointT>(leaf)) {
+      continue;
+    }
+
+    if (has_surface<PointT>(leaf->evals_, surface_eigenvalue_ratio) &&
+        point_plane_distance<PointT>(leaf, p) < surface_neighbor_threshold) {
+      surface->push_back(p);
+    }
+  }
+  return surface;
+}
+
 constexpr double voxel_size = 1.0;
 constexpr int min_points_per_voxel = 10;
-constexpr double edge_eigenvalue_ratio = 8.0;
+constexpr double edge_neighbor_threshold = 0.1;
+constexpr double surface_neighbor_threshold = 1.0;
+constexpr double edge_eigenvalue_ratio = 10.0;
 constexpr double surface_eigenvalue_ratio = 0.1;
 
 int main(int argc, char * argv[])
@@ -118,36 +172,37 @@ int main(int argc, char * argv[])
     return 1;
   }
 
+  using std::chrono::duration_cast;
+  using std::chrono::high_resolution_clock;
+  using std::chrono::milliseconds;
   using PointT = pcl::PointXYZ;
 
   const Eigen::Vector3d voxel_sizes(voxel_size, voxel_size, voxel_size);
 
   const auto pcd_cloud = read_pcd<PointT>(argv[1]);
+
+  const auto start_crop = high_resolution_clock::now();
+
   auto voxels = crop_into_voxels<PointT>(pcd_cloud, voxel_sizes, min_points_per_voxel);
 
-  pcl::PointCloud<PointT>::Ptr edge(new pcl::PointCloud<PointT>);
-  pcl::PointCloud<PointT>::Ptr surface(new pcl::PointCloud<PointT>);
-  for (auto p : *pcd_cloud) {
-    const pcl::VoxelGridCovariance<PointT>::LeafConstPtr leaf = voxels.getLeaf(p);
+  const auto stop_crop = high_resolution_clock::now();
+  const auto start_mapping = high_resolution_clock::now();
 
-    if (leaf == nullptr) {
-      continue;
-    }
+  const auto edge = ExtractEdge(
+    voxels, pcd_cloud,
+    edge_eigenvalue_ratio,
+    edge_neighbor_threshold);
+  const auto surface = ExtractSurface(
+    voxels, pcd_cloud,
+    surface_eigenvalue_ratio,
+    surface_neighbor_threshold);
 
-    if (!has_valid_covariance<PointT>(leaf)) {
-      continue;
-    }
+  const auto stop_mapping = high_resolution_clock::now();
+  const auto duration_crop = duration_cast<milliseconds>(stop_crop - start_crop);
+  const auto duration_mapping = duration_cast<milliseconds>(stop_mapping - start_mapping);
 
-    if (has_edge<PointT>(leaf->evals_, edge_eigenvalue_ratio) &&
-        edge_point_distance<PointT>(leaf, p) < edge_neighbor_threshold) {
-      edge->push_back(p);
-    }
-
-    if (has_surface<PointT>(leaf->evals_, surface_eigenvalue_ratio) &&
-        point_plane_distance<PointT>(leaf, p) < surface_neighbor_threshold) {
-      surface->push_back(p);
-    }
-  }
+  std::cout << "voxel creation : " << duration_crop.count() << " milliseconds" << std::endl;
+  std::cout << "map creation   : " << duration_mapping.count() << " milliseconds" << std::endl;
 
   pcl::io::savePCDFile<PointT>(std::string{"edge.pcd"}, *edge, true);
   pcl::io::savePCDFile<PointT>(std::string{"surface.pcd"}, *surface, true);
